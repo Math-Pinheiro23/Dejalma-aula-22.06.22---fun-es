630438f4064ea295d7a70a6c1760ed35c0c8d8db
curso.ads <curso.ads@LAB06-11>
2022-06-22 20:52:38 -0300

    Adicionar arquivos de projeto.

---
+++ Dejalma aula 22.06.22/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dejalma_aula_22._06._22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma;
+
+            soma = Calcular(30, 20, "+");
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+
+       static public int Calcular(int numero1, int numero2, string operação)
+        {
+            int resultado=0;
+            if (operação == "+")
+            {
+                resultado = numero1 + numero2;
+            }
+            else if(operação == "*")
+            {
+                resultado = numero1 + numero2;
+            }
+            else if (operação == "/")
+            {
+                resultado = numero1 + numero2;
+            }
+            else if (operação == "-")
+            {
+                resultado = numero1 + numero2;
+            }
+            return resultado;
+        }
+    }
+}
